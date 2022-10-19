fe0a71272c3eb8ffc75257f9d18d3d356f36358c
Jacob Grum <jacg@itu.dk>
2022-10-19 15:00:47 +0200

    Fixed null warnings

--- GitInsight/GitPathHelper.cs
+++ GitInsight/GitPathHelper.cs
@@ -4,24 +4,13 @@ public static class GitPathHelper
 {
     public static string GetGitLocalFolder()
     {
-        var projectPath =
-            Path.Combine(Directory.GetParent(typeof(GitCommands).GetTypeInfo().Assembly.Location)?.FullName!);
-        var rootFolder = projectPath.Substring(0, projectPath.IndexOf("\\GitInsight\\", StringComparison.Ordinal) + 12);
-
-        return Path.Combine(rootFolder, ".git");
+        var projectPath =  Directory.GetParent(Directory.GetCurrentDirectory())?.Parent!.Parent!.Parent!.FullName;
+        return Path.Combine(projectPath!, ".git");
     }
 
     public static string GetGitTestFolder()
     {
-        /* Virker ikke med github linter.
-        var projectPath =
-            Path.Combine(Directory.GetParent(typeof(GitCommands).GetTypeInfo().Assembly.Location)?.FullName!);
-        var rootFolder = projectPath.Substring(0, projectPath.IndexOf("\\GitInsight\\", StringComparison.Ordinal) + 12);
-
-        return Path.Combine(rootFolder, "GitInsightTest/Testrepo.git");
-        */
-        
-        var projectPath =  Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
-        return Path.Combine(projectPath, "GitInsightTest/Testrepo.git");
+        var projectPath =  Directory.GetParent(Directory.GetCurrentDirectory())?.Parent!.Parent!.Parent!.FullName;
+        return Path.Combine(projectPath!, "GitInsightTest/Testrepo.git");
     }
 }
