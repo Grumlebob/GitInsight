08852e0ec701e4d2c5bbd47202b033de645cbe7c
Jacob Grum <jacg@itu.dk>
2022-10-25 13:49:39 +0200

    Wooo! First working edition

--- GitInsightUI/MainWindow.xaml.cs
+++ GitInsightUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,19 +19,34 @@ using LibGit2Sharp;
 
 namespace GitInsightUI
 {
+    /* complex stuff, trying to simplify before making work.
     public record AuthorWithCommits(
         string Author,
         ObservableCollection<Commit> commits);
+        */
     
+    public record AuthorWithNumberOfCommits(
+        string AuthorName,
+        int NumberOfCommits);
+    
+
     public partial class MainWindow : Window
     {
+        /* complex stuff, trying to simplify before making work.
         public List<AuthorWithCommits> Authors { get; } = new();
+        */
+
+        public List<AuthorWithNumberOfCommits> AllCommits { get; } = new();
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             InitAuthorList();
+            foreach (var VARIABLE in AllCommits)
+            {
+                Console.WriteLine(VARIABLE);
+            }
         }
 
         public void InitAuthorList()
@@ -38,10 +54,13 @@ namespace GitInsightUI
             var dc = GitCommands.GitLogByAllAuthorsByDate();
             foreach (var author in dc)
             {
+                /* complex stuff, trying to simplify before making work.
                 var authorWithCommits = new AuthorWithCommits(author.Key, new(author.Value));
                 Authors.Add(authorWithCommits);
+                */
+
+                AllCommits.Add(new AuthorWithNumberOfCommits(AuthorName: author.Key, NumberOfCommits:author.Value.Count));
             }
-            
         }
     }
 }
