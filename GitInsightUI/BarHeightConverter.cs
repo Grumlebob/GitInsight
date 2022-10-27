b8b3ac6ec669196ca10f4cd3a8c9a7894d4cd68c
Jacob Grum <jacg@itu.dk>
2022-10-27 11:12:22 +0200

    Commits by date now seperated.

--- GitInsightUI/BarHeightConverter.cs
+++ GitInsightUI/BarHeightConverter.cs
@@ -8,10 +8,12 @@ public class BarHeightConverter : IMultiValueConverter
 {
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
+        /*
         Console.WriteLine("HER ER VI");
         Console.WriteLine("commits:" + values[0]);
         Console.WriteLine("maxScreenHeight" + values[1]);
         Console.WriteLine("maxHighestCommitCount"+parameter);
+        */
        
         if (
             //Actual number of commits
@@ -23,9 +25,11 @@ public class BarHeightConverter : IMultiValueConverter
             // e.g. 50 and a max commit of 75, we want 66% of the max screen height
             && parameter is int maxHighestCommitCount)
         {
-            // Note: *0.98d, is a trick to make it fit within screen and not add a scrollbar.
+            // Note: *0.85d, is a trick to make it fit within screen and not add a scrollbar.
             // ReSharper disable once HeapView.BoxingAllocation
-            return commits / maxHighestCommitCount * maxScreenHeight * 0.98d;
+            //Console.WriteLine("math inspect:" + ((double)commits / (double)maxHighestCommitCount));
+           Console.WriteLine("setHeight:" + (double)commits / (double)maxHighestCommitCount * maxScreenHeight * 0.98d);
+           return (double)commits / (double)maxHighestCommitCount * maxScreenHeight * 0.85d;
         }
 
         throw new InvalidCastException();
