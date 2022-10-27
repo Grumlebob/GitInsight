7b7a68d6d45f59b4b9840f467e1c12b648db082d
Jacob Grum <jacg@itu.dk>
2022-10-27 11:34:44 +0200

    Working selector and scale with window size

---
+++ GitInsightUI/CommitsByAuthorChart.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using GitInsight;
+
+namespace GitInsightUI;
+
+public record AuthorWithNumberOfCommits(
+    string AuthorName,
+    int NumberOfCommits);
+
+public partial class CommitsByAuthorChart : UserControl
+{
+    public List<AuthorWithNumberOfCommits> CommitCountPerAuthor { get; } = new();
+        
+    public static int MaxCommitsPerAuthor { get; set; } = 0;
+
+    public CommitsByAuthorChart()
+    {
+        DataContext = this;
+        InitializeComponent();
+        InitAuthorList();
+    }
+    
+    public void InitAuthorList()
+    {
+        var dc = GitCommands.GitLogByAllAuthorsByDate();
+            
+        foreach (var author in dc)
+        {
+            CommitCountPerAuthor.Add(new AuthorWithNumberOfCommits(AuthorName: author.Key, NumberOfCommits:author.Value.Count));
+        }
+        MaxCommitsPerAuthor = CommitCountPerAuthor.Max(x => x.NumberOfCommits);
+        Console.WriteLine("maxCommits: " + MaxCommitsPerAuthor);
+    }
+}
--- GitInsightUI/MainWindow.xaml.cs
+++ GitInsightUI/MainWindow.xaml.cs
@@ -19,45 +19,20 @@ using LibGit2Sharp;
 
 namespace GitInsightUI
 {
-    public record AuthorWithNumberOfCommits(
-        string AuthorName,
-        int NumberOfCommits);
-    
+ 
 
     public partial class MainWindow : Window
     {
-
-        public List<AuthorWithNumberOfCommits> CommitCountPerAuthor { get; } = new();
-
-        public List<NumberOfCommitsOnDate> CommitCountPerDate { get; } = new();
         
-        public static int MaxCommitsPerDate { get; set; } = 0;
-        
-        public static int MaxCommitsPerAuthor { get; set; } = 0;
-
         public string TabItem1Icon { get; }
         public string TabItem2Icon { get; }
-
         
         public MainWindow()
         {
             TabItem1Icon = ImagePathBuilder.Build("TabIcons/Sample_User_Icon.png");
             TabItem2Icon = ImagePathBuilder.Build("TabIcons/date.png");
             DataContext = this;
-            InitAuthorList();
             InitializeComponent();
-        }
-
-        public void InitAuthorList()
-        {
-            var dc = GitCommands.GitLogByAllAuthorsByDate();
-            
-            foreach (var author in dc)
-            {
-                CommitCountPerAuthor.Add(new AuthorWithNumberOfCommits(AuthorName: author.Key, NumberOfCommits:author.Value.Count));
-            }
-            MaxCommitsPerAuthor = CommitCountPerAuthor.Max(x => x.NumberOfCommits);
-            Console.WriteLine("maxCommits: " + MaxCommitsPerAuthor);
         }
         
     }
