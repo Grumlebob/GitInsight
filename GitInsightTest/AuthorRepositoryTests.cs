1fba2319e983afc7f18784c7f68b35f3271c0bfa
Jacob Grum <jacg@itu.dk>
2022-11-01 14:42:37 +0100

    a tiny bit of testing

--- GitInsightTest/AuthorRepositoryTests.cs
+++ GitInsightTest/AuthorRepositoryTests.cs
@@ -1,11 +1,34 @@
-﻿namespace GitInsightTest;
+﻿using GitInsight.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace GitInsightTest;
 
 public class AuthorRepositoryTests
 {
+    
+    private readonly InsightContext _context;
+    
+    public AuthorRepositoryTests()
+    {
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<InsightContext>();
+        builder.UseSqlite(connection);
+        var context = new InsightContext(builder.Options);
+        context.Database.EnsureCreated();
+        context.SaveChanges();
 
+        _context = context;
+        
+    }
     [Fact]
     public void HowDoesItHandle()
     {
-        true.Should().BeTrue();
+        var allAuthors = _context.Authors;
+
+        allAuthors.Should().BeEmpty();
     }
+    
+    
 }
