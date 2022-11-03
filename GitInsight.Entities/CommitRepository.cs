c9993b39a1857140cebfca28cb174757572a4c5c
alberted115 <aldy@itu.dk>
2022-11-03 09:33:04 +0100

    created repository and test

--- GitInsight.Entities/CommitRepository.cs
+++ GitInsight.Entities/CommitRepository.cs
@@ -2,5 +2,98 @@
 
 public class CommitRepository : ICommitRepository
 {
+    private readonly InsightContext _context;
+
+    public CommitRepository(InsightContext context)
+    {
+        _context = context;
+    }
     
+    public (Commit? commit,Response response) Find(int id)
+    {
+        var entity = _context.Commits.FirstOrDefault(c => c.Id == id);
+        
+        return entity==null ? (null, Response.NotFound) : (entity, Response.Ok);
+    }
+    
+    public (IReadOnlyCollection<Commit>,Response) FindAll()
+    {
+        return (_context.Commits.ToList(),Response.Ok);
+    }
+    
+    public Response Create(Commit commit)
+    {
+        
+        //Check if commit with that Sha already exists
+        if (_context.Commits.FirstOrDefault(c => c.Sha == commit.Sha) != null)
+        {
+            return Response.Conflict;
+        } 
+        
+        //Check for no-existing branch, author, or repository
+        if (_context.Authors.FirstOrDefault(c => c.Id == commit.AuthorId) is null
+                   || _context.Branches.FirstOrDefault(c => c.Id == commit.BranchId) is null
+                   || _context.Repositories.FirstOrDefault(c => c.Id == commit.RepositoryId) is null)
+        {
+            return Response.BadRequest;
+        }
+        
+        _context.Commits.Add(commit);
+        _context.SaveChanges();
+        
+        return Response.Created;
+    }
+    
+    public Response Update(Commit commit)
+    {
+        var entity = _context.Commits.FirstOrDefault(c => c.Id == commit.Id);
+        
+        //Check if commit exits
+        if (entity == null)
+        {
+            return Response.NotFound;
+        } 
+        
+        //Check for no-existing branch, author, or repository
+        if (_context.Authors.FirstOrDefault(c => c.Id == commit.AuthorId) is null
+            || _context.Branches.FirstOrDefault(c => c.Id == commit.BranchId) is null
+            || _context.Repositories.FirstOrDefault(c => c.Id == commit.RepositoryId) is null)
+        {
+            return Response.BadRequest;
+        }
+        
+        //Check if any information actually have been updated
+        if (   entity.AuthorId==commit.AuthorId 
+            && entity.BranchId==commit.BranchId
+            && entity.RepositoryId==commit.RepositoryId
+            && entity.Sha==commit.Sha
+            && entity.Date==commit.Date
+            && entity.Tag==commit.Tag)
+        {
+            return Response.BadRequest;
+        }
+
+        //Update commit
+        entity.AuthorId = commit.AuthorId; 
+        entity.BranchId=commit.BranchId;
+        entity.RepositoryId=commit.RepositoryId;
+        entity.Sha=commit.Sha;
+        entity.Date=commit.Date;
+        entity.Tag = commit.Tag;
+        _context.SaveChanges();
+        
+        return Response.Ok;
+    }
+    
+    public Response Delete(int id)
+    {
+        var entity = _context.Commits.FirstOrDefault(c => c.Id == id);
+        
+        if (entity == null) return Response.NotFound;
+
+        _context.Commits.Remove(entity);
+        _context.SaveChanges();
+
+        return Response.Deleted;
+    }
 }
--- GitInsightTest/CommitRepositoryTest.cs
+++ GitInsightTest/CommitRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using GitInsight.Entities;
+﻿using GitInsight.Core;
+using GitInsight.Entities;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Branch = GitInsight.Entities.Branch;
@@ -7,19 +8,152 @@ namespace GitInsightTest;
 
 public class CommitRepositoryTest
 {
-    
     private readonly InsightContext _context;
-    
+    private readonly CommitRepository _repository;
+
     public CommitRepositoryTest()
     {
-        _context = SetupTests.Setup();
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<InsightContext>();
+        builder.UseSqlite(connection);
+        var context = new InsightContext(builder.Options);
+
+        context.Database.EnsureCreated();
+        
+        _context = context;
+        _repository = new CommitRepository(_context);
+
+        _context.Authors.Add(new Author{ Name = "Søren", Email = "søren@gmail.dk" });
+        _context.Authors.Add(new Author{ Name = "Per", Email = "per@gmail.dk" });
+        _context.SaveChanges();
+
+        
+        _context.Repositories.Add(new GitInsight.Entities.Repository{ Name = "repo1", Path = "idk/idk"});
+        _context.Repositories.Add(new GitInsight.Entities.Repository{ Name = "repo2", Path = "idc/idc"});
+        _context.SaveChanges();
+
+        _context.Branches.Add(new Branch{ Name = "branch1", Sha= "huhu", Path = "origin/idk", RepositoryId = 1});
+        _context.Branches.Add(new Branch{ Name = "branch2", Sha= "hihi", Path = "origin/idc", RepositoryId = 2});
+        _context.SaveChanges();
+
+        _context.Commits.Add(new GitInsight.Entities.Commit{Sha= "treg", Tag = "1.2.3", AuthorId = 1, BranchId = 1, RepositoryId = 1, Date = DateTimeOffset.Now});
+        _context.Commits.Add(new GitInsight.Entities.Commit{Sha= "heck", Tag = "1.1.2", AuthorId = 2, BranchId = 2, RepositoryId = 2, Date = DateTimeOffset.Now});
+        _context.Commits.Add(new GitInsight.Entities.Commit{Sha= "tger", Tag = "1.1.4", AuthorId = 1, BranchId = 2, RepositoryId = 2, Date = DateTimeOffset.Now});
+
+        
+        _context.SaveChanges();
+
     }
  
     [Fact]
-    public void SimpleCommitObject()
+    public void Find_id_1_return_ok()
     {
-        GitInsight.Entities.Commit a = new GitInsight.Entities.Commit();
-        a.Id = 1;
-        a.Sha = "wafwa";
+        var (commit,response) = _repository.Find(1);
+
+        response.Should().Be(Response.Ok);
+
+        commit.Sha.Should().Be("treg");
     }
+    
+    [Fact]
+    public void Find_id_4_return_not_found()
+    {
+        var (commit,response) = _repository.Find(4);
+
+        response.Should().Be(Response.NotFound);
+    }
+    
+    
+    [Fact]
+    public void Find_id_all_return_ok()
+    {
+        var (commits,response) = _repository.FindAll();
+
+        response.Should().Be(Response.Ok);
+        commits.Count.Should().Be(3);
+    }
+    
+    
+    [Fact]
+    public void Create_return_created()
+    {
+        var response = _repository.Create(new GitInsight.Entities.Commit{Sha= "heyuo", Tag = "3.2.5", AuthorId = 2, BranchId = 1, RepositoryId = 1, Date = DateTimeOffset.Now});
+
+        response.Should().Be(Response.Created);
+        _repository.Find(4).commit.Sha.Should().Be("heyuo");
+    }
+    
+    
+    [Fact]
+    public void Create_return_conflict_because_duplicate_sha()
+    {
+        var response = _repository.Create(new GitInsight.Entities.Commit{Sha= "treg", Tag = "3.2.5", AuthorId = 2, BranchId = 1, RepositoryId = 1, Date = DateTimeOffset.Now});
+
+        response.Should().Be(Response.Conflict);
+        _repository.FindAll().Item1.Count.Should().Be(3);
+    }
+    
+    [Fact]
+    public void Create_return_badRequest_because_nonExisting_author()
+    {
+        var response = _repository.Create(new GitInsight.Entities.Commit{Sha= "heyuo", Tag = "3.2.5", AuthorId = 3, BranchId = 1, RepositoryId = 1, Date = DateTimeOffset.Now});
+
+        response.Should().Be(Response.BadRequest);
+        _repository.FindAll().Item1.Count.Should().Be(3);
+    }
+    
+    
+    [Fact]
+    public void Update_id_1_return_ok()
+    {
+        var response = _repository.Update(new GitInsight.Entities.Commit{Id=1, Sha= "treg", Tag = "1.2.5", AuthorId = 1, BranchId = 1, RepositoryId = 1, Date = DateTimeOffset.Now});
+
+        response.Should().Be(Response.Ok);
+        _repository.Find(1).commit.Tag.Should().Be("1.2.5");
+    }
+    
+    
+    [Fact]
+    public void Update_id_4_return_notfound()
+    {
+        var response = _repository.Update(new GitInsight.Entities.Commit{Id=4, Sha= "hjgk", Tag = "1.2.5", AuthorId = 2, BranchId = 1, RepositoryId = 1, Date = DateTimeOffset.Now});
+
+        response.Should().Be(Response.NotFound);
+    }
+    
+    [Fact]
+    public void Update_return_badRequest_because_nonExisting_repo()
+    {
+        var response = _repository.Update(new GitInsight.Entities.Commit{Id = 2, Sha= "heck", Tag = "1.1.2", AuthorId = 2, BranchId = 2, RepositoryId = 3, Date = DateTimeOffset.Now});
+
+        response.Should().Be(Response.BadRequest);
+        _repository.Find(2).commit.RepositoryId.Should().Be(2);
+    }
+    
+    [Fact]
+    public void Update_return_badRequest_because_nothing_is_changed()
+    {
+        var response = _repository.Update(new GitInsight.Entities.Commit{Id=3 ,Sha= "tger", Tag = "1.1.4", AuthorId = 1, BranchId = 2, RepositoryId = 2, Date = _repository.Find(3).commit.Date});
+
+        response.Should().Be(Response.BadRequest);
+    }
+    
+    [Fact]
+    public void Deleted_id_1_return_deleted()
+    {
+        var response = _repository.Delete(1);
+
+        response.Should().Be(Response.Deleted);
+        _repository.Find(1).response.Should().Be(Response.NotFound);
+    }
+    
+    [Fact]
+    public void Deleted_id_5_return_notFound()
+    {
+        var response = _repository.Delete(4);
+
+        response.Should().Be(Response.NotFound);
+    }
+    
 }
