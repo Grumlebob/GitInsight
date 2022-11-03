d270db85cb8ade74f5afe5fe0bd84338323ba404
Jacob Grum <jacg@itu.dk>
2022-11-03 15:10:08 +0100

    Author repository pattern with full testing

---
+++ GitInsight.Core/AuthorDto.cs
@@ -0,0 +1,5 @@
+﻿namespace GitInsight.Core;
+
+public record AuthorDto(int Id, string Name, string Email, List<int> CommitIds, List<int> RepositoryIds);
+
+public record AuthorCreateDto(string Name, string Email, List<int> CommitIds, List<int> RepositoryIds);
---
+++ GitInsight.Core/IAuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace GitInsight.Core;
+
+public interface IAuthorRepository
+{
+    Task<(AuthorDto?, Response)> FindAuthorAsync(int id);
+    Task<(List<AuthorDto>?, Response response)> FindAllAuthorsAsync();
+    Task<(AuthorDto?, Response)> CreateAuthorAsync(AuthorCreateDto authorCreateDto);
+    Task<Response> DeleteAuthorAsync(int id);
+    Task<Response> UpdateAuthorAsync(AuthorDto authorDto);
+    Task<(List<AuthorDto>?, Response)> FindAuthorsByNameAsync(string name);
+    Task<(List<AuthorDto>?, Response)> FindAuthorsByEmailAsync(string email);
+    Task<(List<AuthorDto>?, Response)> FindAuthorsByRepositoryIdAsync(int repositoryId);
+    Task<(List<AuthorDto>?, Response)> FindAuthorsByCommitIdAsync(int commitId);
+}
