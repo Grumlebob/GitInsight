7bb4f56e32a843895502bbf5e5aa3383f837e66f
alberted115 <aldy@itu.dk>
2022-11-03 10:37:49 +0100

    added interface

--- GitInsight.Core/ICommitRepository.cs
+++ GitInsight.Core/ICommitRepository.cs
@@ -2,5 +2,9 @@
 
 public interface ICommitRepository
 {
-    
+    (CommitDTO? commit,Response response) Find(int id);
+    (IReadOnlyCollection<CommitDTO> commits, Response response) FindAll();
+    (Response response, CommitDTO? commit) Create(CommitCreateDTO DTO);
+    (Response response, CommitDTO? commit) Update(CommitDTO commit);
+    Response Delete(int id);
 }
