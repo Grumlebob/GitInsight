208c1aa9e357eca18ee08ea0a9f59a13e6cda23b
Grumlebob <80719417+Grumlebob@users.noreply.github.com>
2022-11-03 14:13:22 +0100

    Update AuthorRepository.cs

--- GitInsight.Entities/AuthorRepository.cs
+++ GitInsight.Entities/AuthorRepository.cs
@@ -4,32 +4,5 @@ namespace GitInsight.Entities;
 
 public class AuthorRepository : IAuthorRepository
 {
-    private readonly InsightContext _context;
-
-    public AuthorRepository(InsightContext context)
-    {
-        _context = context;
-    }
-
-    public async Task<(AuthorDto?, Response)> FindAuthorAsync(int id)
-    {
-        var response = new Response();
-        
-        var author = await _context.Authors
-            .Include(a => a.Commits)
-            .FirstOrDefaultAsync(a =>  a.Id == id);
-
-        if (author is null)
-        {
-            response = Response.NotFound;
-            return (null, response);
-        }
-        else
-        {
-            response = Response.Ok;
-            return (new AuthorDto(Id: author.Id, Name: author.Name, Email: author.Email), response);
-        }
-    }
-
-    public record AuthorDto(int Id, string Name, string Email);
+    
 }
