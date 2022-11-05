5044e96c566bc4ec4890916a59fcfb1e01d5efa0
alberted115 <aldy@itu.dk>
2022-11-05 08:53:01 +0100

    finished configuration for commit

--- GitInsight.Entities/Commit.cs
+++ GitInsight.Entities/Commit.cs
@@ -9,21 +9,16 @@ public class Commit
     public string Sha { get; set; }
     public DateTimeOffset Date { get; set; }
     public string? Tag { get; set; }
-    
+
     public Author Author { get; set; }
     public int AuthorId { get; set; }
-    
+
     public Branch Branch { get; set; }
     public int BranchId { get; set; }
-    
+
     public Repository Repository { get; set; }
     public int RepositoryId { get; set; }
-    
-    public Commit() 
-    {
-       
-    }
-    
+
 }
 
 public class CommitConfigurations : IEntityTypeConfiguration<Commit>
@@ -31,5 +26,14 @@ public class CommitConfigurations : IEntityTypeConfiguration<Commit>
     public void Configure(EntityTypeBuilder<Commit> builder)
     {
         builder.HasKey(a => a.Id);
+        
+        builder.HasOne(a => a.Repository)
+            .WithMany(a => a.Commits);
+        
+        builder.HasOne(a => a.Author)
+            .WithMany(a => a.Commits);
+        
+        builder.HasOne(a => a.Branch)
+            .WithMany(a => a.Commits);
     }
 }
