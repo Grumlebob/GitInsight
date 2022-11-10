c3e11f11e9244f89e088d415e9ae82c987060599
Jacob Grum <jacg@itu.dk>
2022-11-10 08:21:23 +0100

    Added mutual exclusion to zipping logic

--- GitInsightTest/SetupTests.cs
+++ GitInsightTest/SetupTests.cs
@@ -5,8 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GitInsightTest;
 
-public class SetupTests
+public static class SetupTests
 {
+    public static bool _hasBeenUnzipped = false;
+
     public static (SqliteConnection, InsightContext) Setup()
     {
         EnsureZipIsUnzippedTesting();
@@ -19,25 +21,30 @@ public class SetupTests
         context.SaveChanges();
         return (connection, context);
     }
-    
+
     public static void EnsureZipIsUnzippedTesting() //Only works in testing directory
     {
-        var projectPath =  Directory.GetParent(Directory.GetCurrentDirectory())?.Parent!.Parent!.Parent!.FullName;
-        
-        string unzippedFolder = Path.Combine(projectPath! + GetRelativeTestFolder());
-        string zippedFolder = Path.Combine(projectPath! + @"/GitInsightTest/TestResources/Zipped/Testrepo.git.zip");
-        string destinationFolder = Path.Combine(projectPath! + @"/GitInsightTest/TestResources/Unzipped/");
-        
-        if (!Directory.Exists(unzippedFolder))
+        if (_hasBeenUnzipped) return;
+        var projectPath = "";
+        lock (projectPath)
         {
-            ZipFile.ExtractToDirectory(
-                zippedFolder, 
-                destinationFolder,true);
-            Console.WriteLine("Testrepo.git unzipped");
-        }
-        else
-        {
-            Console.WriteLine("Testrepo.git already unzipped");
+            projectPath = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent!.Parent!.Parent!.FullName;
+            string unzippedFolder = Path.Combine(projectPath! + GetRelativeTestFolder());
+            string zippedFolder = Path.Combine(projectPath! + @"/GitInsightTest/TestResources/Zipped/Testrepo.git.zip");
+            string destinationFolder = Path.Combine(projectPath! + @"/GitInsightTest/TestResources/Unzipped/");
+            
+            if (!Directory.Exists(unzippedFolder))
+            {
+                ZipFile.ExtractToDirectory(
+                    zippedFolder,
+                    destinationFolder, true);
+                _hasBeenUnzipped = true;
+                Console.WriteLine("Testrepo.git unzipped");
+            }
+            else
+            {
+                Console.WriteLine("Testrepo.git already unzipped");
+            }
         }
     }
 }
