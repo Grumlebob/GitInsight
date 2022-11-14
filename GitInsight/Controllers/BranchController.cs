b0c7940a17209b01992dd4703e42df059e36f66c
Thomas Sørup Bavnhøj <tbav@itu.dk>
2022-11-14 15:56:15 +0100

    BranchController with CRUD

--- GitInsight/Controllers/BranchController.cs
+++ GitInsight/Controllers/BranchController.cs
@@ -1,75 +1,75 @@
-﻿// using GitInsight.Core;
-// using GitInsight.Entities;
-// using Microsoft.AspNetCore.Mvc;
+﻿using GitInsight.Core;
+using GitInsight.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace GitInsight.Controllers;
+namespace GitInsight.Controllers;
 
-// [ApiController]
-// [Route("[controller]")]
-// public class BranchController : ControllerBase
+[ApiController]
+[Route("[controller]")]
+public class BranchController : ControllerBase
 
-// {
-//     private readonly IBranchRepository _branchRepository;
+{
+    private readonly IBranchRepository _branchRepository;
 
-//     public BranchController(InsightContext context)
-//     {
-//         _branchRepository = new BranchRepository(context);
-//     }
+    public BranchController(InsightContext context)
+    {
+        _branchRepository = new BranchRepository(context);
+    }
 
-//     [HttpGet]
-//     [ProducesResponseType(StatusCodes.Status404NotFound)]
-//     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Entities.Branch>))]
-//     public async Task<IActionResult> GetAllBranches()
-//     {
-//         var list = await _branchRepository.FindAllAsync();
-//         if (response == Core.Response.Ok) return Ok(list);
-//         return NotFound();
-//     }
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Entities.Branch>))]
+    public async Task<IActionResult> GetAllBranches()
+    {
+        var (list, response) = await _branchRepository.FindAllAsync();
+        if (response == Core.Response.Ok) return Ok(list);
+        return NotFound();
+    }
 
-//     [HttpGet]
-//     [Route("{id}")]
-//     [ProducesResponseType(StatusCodes.Status404NotFound)]
-//     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Author))]
-//     public async Task<IActionResult> GetAuthorById(int id)
-//     {
-//         var (authorDto, response) = await _branchRepository.FindAsync(id);
-//         if (response == Core.Response.NotFound) return NotFound();
-//         return Ok(authorDto);
-//     }
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Entities.Branch))]
+    public async Task<IActionResult> GetBranchById(int id)
+    {
+        var (branchDto, response) = await _branchRepository.FindAsync(id);
+        if (response == Core.Response.NotFound) return NotFound();
+        return Ok(branchDto);
+    }
 
-//     [HttpPost]
-//     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-//     [ProducesResponseType(StatusCodes.Status409Conflict)]
-//     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Author))]
-//     public async Task<IActionResult> CreateAuthor(AuthorCreateDto author)
-//     {
-//         var (authorDto, response) = await _branchRepository.CreateAsync(author);
-//         if (response == Core.Response.BadRequest) return BadRequest();
-//         if (response == Core.Response.Conflict) return Conflict(authorDto);
-//         if (response == Core.Response.Created) return CreatedAtAction(nameof(GetAuthorById), new { id = authorDto.Id }, authorDto);
-//         return Ok(authorDto);
-//     }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Entities.Branch))]
+    public async Task<IActionResult> CreateBranch(BranchCreateDto branch)
+    {
+        var (branchDto, response) = await _branchRepository.CreateAsync(branch);
+        if (response == Core.Response.BadRequest) return BadRequest();
+        if (response == Core.Response.Conflict) return Conflict(branchDto);
+        if (response == Core.Response.Created) return CreatedAtAction(nameof(GetBranchById), new { id = branchDto.Id }, branchDto);
+        return Ok(branchDto);
+    }
 
-//     [HttpPut]
-//     [ProducesResponseType(StatusCodes.Status404NotFound)]
-//     [ProducesResponseType(StatusCodes.Status200OK)]
-//     public async Task<IActionResult> UpdateAuthor(AuthorDto author)
-//     {
-//         var response = await _branchRepository.UpdateAsync(author);
-//         if (response == Core.Response.NotFound) return NotFound();
-//         return Ok();
-//     }
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateBranch(BranchDto branch)
+    {
+        var response = await _branchRepository.UpdateAsync(branch);
+        if (response == Core.Response.NotFound) return NotFound();
+        return Ok();
+    }
 
-//     [HttpDelete]
-//     [Route("{id}")]
-//     [ProducesResponseType(StatusCodes.Status404NotFound)]
-//     [ProducesResponseType(StatusCodes.Status204NoContent)]
-//     public async Task<IActionResult> DeleteAuthor(int id)
-//     {
-//         var response = await _branchRepository.DeleteAsync(id);
-//         if (response == Core.Response.NotFound) return NotFound();
-//         return NoContent();
-//     }
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteBranch(int id)
+    {
+        var response = await _branchRepository.DeleteAsync(id);
+        if (response == Core.Response.NotFound) return NotFound();
+        return NoContent();
+    }
 
 
-// }
+}
