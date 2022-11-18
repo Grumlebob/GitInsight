d4e6446768f7d3c9a213da37276cc3716ae6a83e
alberted115 <aldy@itu.dk>
2022-11-18 11:31:58 +0100

    connect web to api attempt

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+
 var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
 
 builder.Services.AddDbContext<InsightContext>(o =>
